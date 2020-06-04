3085fc83346fb9b7dda558d4a3440c42462a38cb
Carly L <carlylynnla@gmail.com>
2020-06-04 16:26:22 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,25 @@
 # your code goes here
+class Person
+  attr_accessor :happiness, :hygiene, :bank_account
+  attr_reader :name
+  attr_writer
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  
+  def clean?
+    if @hygiene > 7
+      TRUE
+    end
+  end
+  
+  def happy?
+    if @happiness > 7
+      TRUE
+    end
+  end
+  
+end
